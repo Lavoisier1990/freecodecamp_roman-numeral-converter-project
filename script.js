dfc1e731323ad598f15409ab6d67bac49234d60d
Takunda Kangai <kangaitakunda@gmail.com>
2024-06-14 20:09:40 +0200

    Complete the main algorithm of the Roman Converter.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const inputNumber = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const outputDiv = document.getElementById("output");
+
+const convertToRoman = function(inputNumber){
+ const romanNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "1"];
+ const arabicNumerals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+ let romanResult = "";
+
+ for(let i = 0; i < arabicNumerals.length; i++){
+    while(inputNumber.value >= arabicNumerals[i]){
+        romanResult += romanNumerals[i];
+        inputNumber.value -= arabicNumerals[i];
+    }
+ }
+}
